06c5620c2d277a992fd3cc84d21e5d640298f2f2
Sk Hasibul Alam <sk.hasibul.alam@gmail.com>
2021-10-07 02:23:21 +0600

    Update EncrDecr.m

--- EncrDecr.m
+++ EncrDecr.m
@@ -1,4 +1,3 @@
-%% 
 % ENCRYPTION part starts here-----------------------------------------
 a1=imread('tank.jpg');% Use your desired plain Image file in this line
 
@@ -15,7 +14,6 @@ else
     nlayer=1;% It has only one layer (Rare case)
 end
 
-c=zeros(2,2);
 block=zeros(size(a1,1),size(a1,2));
 k1=zeros(size(a1,1),size(a1,2),3);
 
@@ -43,7 +41,6 @@ imwrite(k1,'encrypted.png','compression','none','mode','lossless');
 % Encryption is done, new file saved as encrypted.png
 % ENCRYPTION part ends here-----------------------------------------
 
-%% 
 % DECRYPTION part starts here---------------------------------------
 a2=imread('encrypted.png');% Use the encrypted Image file in this line
 
@@ -54,7 +51,6 @@ else
     nlayer=1;% It has only one layer (Rare case)
 end
 
-c=zeros(2,2);
 block=zeros(size(a2,1),size(a2,2));
 k2=zeros(size(a1,1),size(a1,2),3);
 
@@ -81,3 +77,111 @@ k2=uint8(k2);
 imwrite(k2,'decrypted.png','compression','none','mode','lossless');
 % Decryption is done, new file saved as decrypted.png
 % DECRYPTION part ends here------------------------------------------
+
+% ENCRYPTION subroutine
+function [out]=encr(a,r)
+key={'f4' '4d' '7a' '15'...
+    'c9' '99' '1a' '9f'...
+    '85' '40' 'ff' '23'...
+    '98' 'a8' 'b2' 'b1'...
+    '52' 'b7' '5d' '11'...
+    '54' 'f3' 'f7' 'd3'...
+    'c2' 'cc' '2c' '22'...
+    '10' '16' '23' '30'};
+sbox=[ 12 5 15 0 14 4 9 7 2 10 3 8 6 11 1 13;
+    10 4 1 12 5 2 14 3 7 11 6 13 8 0 9 15;
+    5 0 10 1 4 9 8 11 15 2 6 12 3 13 14 7;
+    15 4 10 0 12 8 3 1 7 9 13 11 5 14 2 6];
+keybin=dec2bin(hex2dec(key),8);
+keybin=(bin2dec(keybin(:,7:8)))';
+b= zeros(1,4);
+scdxor= zeros(1,4);
+sub= zeros(1,8);
+for i=1:4
+    b(i)= bitxor(a(i),hex2dec(key(r+i-1)));
+end
+b=dec2bin(b,8);
+c=[b(1,1:4); b(1,5:8); b(2,1:4); b(2,5:8);
+    b(3,1:4); b(3,5:8); b(4,1:4); b(4,5:8)];
+c=(bin2dec(c))';
+for i=1:8
+    sub(i)=sbox(keybin(r+i-1)+1,c(i)+1);
+end
+sub=dec2bin(sub,4);
+subcat=[sub(1,:) sub(2,:); sub(3,:) sub(4,:);
+    sub(5,:) sub(6,:); sub(7,:) sub(8,:)];
+subcat=(bin2dec(subcat));
+MDS=[ 2 3 1 1;
+    1 2 3 1;
+    1 1 2 3;
+    3 1 1 2];
+subcat_gf=gf(subcat,8);
+MDS_gf=gf(MDS,8);
+out_gf=MDS_gf*subcat_gf;
+subcat=out_gf.x;
+subcat=circshift(subcat,-2);
+subcat=dec2bin(subcat,8);
+for i=1:4
+    subcat(i,:)=circshift(subcat(i,:),-r);
+end
+subcat=(bin2dec(subcat))';
+for i=1:4
+    scdxor(i)=bitxor(subcat(4-i+1),hex2dec(key(32-r-i+2)));
+end
+out=scdxor;
+return
+end
+
+%DECRYPTION subroutine
+function [out]=decr(scdxor,r)
+key={'f4' '4d' '7a' '15'...
+    'c9' '99' '1a' '9f'...
+    '85' '40' 'ff' '23'...
+    '98' 'a8' 'b2' 'b1'...
+    '52' 'b7' '5d' '11'...
+    '54' 'f3' 'f7' 'd3'...
+    'c2' 'cc' '2c' '22'...
+    '10' '16' '23' '30'};
+sbox=[ 3 14 8 10 5 1 12 7 11 6 9 13 0 15 4 2;
+    13 2 5 7 1 4 10 8 12 14 0 9 3 11 6 15;
+    1 3 9 12 4 0 10 15 6 5 2 7 11 13 14 8;
+    3 7 14 6 1 12 15 8 5 9 2 11 4 10 13 0];
+keybin=dec2bin(hex2dec(key),8);
+keybin=(bin2dec(keybin(:,7:8)))';
+a=zeros (1,4);
+c=zeros (1,8);
+subcat=zeros (1,4);
+for i=1:4
+    subcat(4-i+1)=bitxor(scdxor(i),hex2dec(key(32-r-i+2)));
+end
+subcat=dec2bin(subcat,8);
+for i=1:4
+    subcat(i,:)=circshift(subcat(i,:),r);
+end
+subcat=(bin2dec(subcat))';
+subcat=circshift(subcat,2);
+MDS=[ 14 11 13 9;
+    9 14 11 13;
+    13 9 14 11;
+    11 13 9 14];
+subcat_gf=gf(subcat',8);
+MDS_gf=gf(MDS,8);
+out_gf=MDS_gf*subcat_gf;
+subcat=out_gf.x;
+subcat=dec2bin(subcat,8);
+sub=[subcat(1,1:4); subcat(1,5:8); subcat(2,1:4); subcat(2,5:8);
+    subcat(3,1:4); subcat(3,5:8); subcat(4,1:4); subcat(4,5:8)];
+sub=(bin2dec(sub))';
+for i=1:8
+    c(i)=sbox(keybin(r+i-1)+1,sub(i)+1);
+end
+c=dec2bin(c,4);
+b=[c(1,:) c(2,:); c(3,:) c(4,:);
+    c(5,:) c(6,:); c(7,:) c(8,:)];
+b=(bin2dec(b))';
+for i=1:4
+    a(i)=bitxor(b(i),hex2dec(key(r+i-1)));
+end
+out=a;
+return
+end
